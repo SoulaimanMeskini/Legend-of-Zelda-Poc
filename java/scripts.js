c36da5da8eb2528b4fa51002f96956e4ef7f1016
Soulaiman Meskini <65052301+SoulaimanMeskini@users.noreply.github.com>
2021-12-16 15:29:15 +0100

    Full Poc with readme

---
+++ java/scripts.js
@@ -0,0 +1,37 @@
+/* cursor*/
+
+const cursor = document.querySelector('.pointer');
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
+// loader // 
+var loader
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.03)
+        }, 100);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementsById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1)
+});
